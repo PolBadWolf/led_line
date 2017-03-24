41d87fdd83136972d8219df1be31e1efc64e63de
Marat Gumerov <gm.gameoff@gmail.com>
2017-03-24 15:18:36 +0600

    Header class ws

--- led_reg/main.cpp
+++ led_reg/main.cpp
@@ -11,10 +11,17 @@
 
 int main(void)
 {
-	ws2812::Init();
+	//DDRB = 0xff;
+	//PORTB = 0;
+	__port(DDRB).bit7 = 1;
+	//ws2812::Init();
     /* Replace with your application code */
     while (1) 
     {
+		__port(PORTB).bit7 = 0;
+		__delay_ms(500);
+		__port(PORTB).bit7 = 1;
+		__delay_ms(500);
 		//ws2812::SendBit((uint8_t)1);
     }
 }
